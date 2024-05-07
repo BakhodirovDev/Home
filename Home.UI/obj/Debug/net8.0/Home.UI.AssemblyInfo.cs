d75cf2f25a9c779ac257e08099ab3b0749728bf1
BakhodirovDev <bbahodirov.dev@gmail.com>
2024-05-07 11:06:38 +0500

    add git ignore

--- Home.UI/obj/Debug/net8.0/Home.UI.AssemblyInfo.cs
+++ Home.UI/obj/Debug/net8.0/Home.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Home.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f68a7041c8404b82e6c9a9ed32564476add4abf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Home.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Home.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
